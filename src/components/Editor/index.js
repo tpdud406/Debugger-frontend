cb00f77bc6a965c113c18c67c8db6f065c0d35ed
tpdud406 <tpdud406@naver.com>
2022-11-17 12:46:52 +0900

    feat: 코드미러 적용(옵션,핸들러 셋팅 전)

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,9 +1,37 @@
 import styled from "styled-components";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import { useState } from "react";
+require("codemirror/mode/javascript/javascript");
+require("codemirror/lib/codemirror.css");
+
+const INIT_VALUE = `function solution(n) {
+  //Code...
+}`;
 
 function Editor() {
+  const [value, setValue] = useState(INIT_VALUE);
+  const onChange = (editor, data, value) => {
+    setValue({
+      value,
+    });
+  };
   return (
     <Wrapper>
       <Textarea defaultValue={"코드미러 에디터"} />
+      <CodeMirror
+        value={value}
+        options={{
+          mode: "javascript",
+          lineNumbers: true,
+          lint: true,
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setValue({
+            value,
+          });
+        }}
+        onChange={onChange}
+      />
     </Wrapper>
   );
 }

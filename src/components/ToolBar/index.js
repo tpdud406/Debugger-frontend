b8c23a9811f6f4e7b88b06fbc0ea19d523d4823f
tpdud406 <tpdud406@gmail.com>
2022-11-21 11:34:40 +0900

    feat: 소켓셋팅, 연결, 컨텍스트화 (#4)
    * feat:소켓셋팅, backend랑 연결
    * feat: 소켓을 여러 컴포넌트에서 사용하기 위해 소켓 컨텍스트 처리

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -6,14 +6,35 @@ import {
   VscDebugStepOut,
   VscDebugStepOver,
 } from "react-icons/vsc";
+import { useContext } from "react";
+import { SocketContext } from "../../context/socket";
 
 function ToolBar() {
+  const socket = useContext(SocketContext);
+  const emitHandler = (e) => {
+    const { name } = e.currentTarget;
+
+    console.log(name);
+    socket.emit(`${name}`, `hi I'm ${name}`);
+  };
+
   return (
     <Wrapper>
-      {<VscDebugStart /> || <VscDebugPause />}
-      <VscDebugStepInto />
-      <VscDebugStepOut />
-      <VscDebugStepOver />
+      <Button type="button" name="run">
+        <VscDebugStart />
+      </Button>
+      <Button type="button" name="paused" onClick={emitHandler}>
+        <VscDebugPause />
+      </Button>
+      <Button type="button" name="stepInto" onClick={emitHandler}>
+        <VscDebugStepInto />
+      </Button>
+      <Button type="button" name="stepOut" onClick={emitHandler}>
+        <VscDebugStepOut />
+      </Button>
+      <Button type="button" name="stepOver" onClick={emitHandler}>
+        <VscDebugStepOver />
+      </Button>
     </Wrapper>
   );
 }
@@ -23,13 +44,29 @@ export default ToolBar;
 const Wrapper = styled.div`
   display: flex;
   flex-basis: auto;
-  background: #00154f;
-  color: #f4af1b;
   width: 100%;
   height: 60px;
   border: 3px solid #f2bc94;
   border-radius: 20px;
   justify-content: space-evenly;
   padding-top: 10px;
-  font-size: 50px;
+`;
+
+const Button = styled.button`
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 15px;
+  background: #00154f;
+  color: grey;
+  font-size: 40px;
+  cursor: pointer;
+
+  &:hover {
+    color: #f4af1b;
+  }
+
+  &:active {
+    transform: translateY(4px);
+  }
 `;

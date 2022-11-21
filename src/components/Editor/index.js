8e3b60a22c975734777dbf64d53e3a6fa4067b3f
tpdud406 <tpdud406@gmail.com>
2022-11-21 11:24:34 +0900

    feat: 코드미러 중단점 설정

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,22 +1,74 @@
 import styled from "styled-components";
 import { Controlled as CodeMirror } from "react-codemirror2-react-17";
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material-darker.css";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/addon/edit/matchbrackets";
 import "codemirror/addon/edit/closebrackets";
+import { gutterMarker } from "../../services/gutterMarker";
 
 const INIT_VALUE = `function solution(n) {
   //Code...
 }`;
 
-function Editor() {
+function Editor(ref) {
+  // const editorRef = useRef();
   const [code, setCode] = useState(INIT_VALUE);
+  const editorState = {
+    lastScope: null,
+    lastPosition: null,
+    lastLine: null,
+  };
 
+  const cleanUp = () => {
+    const { lastScope, lastPosition, lastLine } = editorState;
+
+    if (lastScope) {
+      lastScope.clear();
+    }
+
+    if (lastPosition) {
+      lastPosition.clear();
+    }
+
+    if (lastLine) {
+      lastLine.clear();
+    }
+
+    if (ref.current) {
+      ref.current.clearGutter("breakpoints");
+    }
+  };
+
+  const disableEditing = () => {
+    ref.current.setOption("readOnly", true);
+  };
+
+  const enableEditing = () => {
+    ref.current.setOption("readOnly", false);
+  };
+
+  useEffect(() => {
+    // if (editorRef) {
+    //   ref.current = editorRef.current;
+    // }
+    ref.current.focus();
+  });
+
+  const gutterHandler = (cm, lineNumber, gutter, event) => {
+    const info = cm.lineInfo(lineNumber);
+
+    cm.setGutterMarker(
+      lineNumber,
+      "breakpoints",
+      info.gutterMarkers ? null : gutterMarker(),
+    );
+  };
   return (
     <Wrapper>
       <CodeMirror
+        ref={ref}
         value={String(code)}
         options={{
           mode: "javascript",
@@ -26,16 +78,18 @@ function Editor() {
           lint: true,
           matchBrackets: true,
           autoCloseBrackets: true,
+          gutters: ["Codemirror-linenumbers", "breakpoints"],
         }}
         onBeforeChange={(editor, data, value) => {
           setCode(value);
         }}
+        onGutterClick={gutterHandler}
       />
     </Wrapper>
   );
 }
 
-export default Editor;
+export default React.forwardRef(Editor);
 
 const Wrapper = styled.form`
   background: black;
---
+++ src/services/gutterMarker.js
@@ -0,0 +1,6 @@
+export function gutterMarker() {
+  const marker = document.createElement("div");
+  marker.style.color = "#822";
+  marker.innerHTML = "●";
+  return marker;
+}
